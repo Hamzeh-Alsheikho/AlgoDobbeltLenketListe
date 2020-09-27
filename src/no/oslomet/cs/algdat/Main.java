38a79a7d80da3fada831eb4dce3d501fde4c8c37
Hitomi-jp <hitomi.hoshino@gmail.com>
2020-09-27 14:40:43 +0200

    Merge branch 'master' of C:\Users\hitom\DATS2300-1_20H_AlgoritmerOg Datastrukturer\Oblig2_NEW with conflicts.

--- src/no/oslomet/cs/algdat/Main.java
+++ src/no/oslomet/cs/algdat/Main.java
@@ -4,6 +4,20 @@ import java.util.Arrays;
 
 public class Main {
     public static void main (String[] args) {
+       /* DobbeltLenketListe<Integer> integerDobbeltLenketListe = new DobbeltLenketListe<>();
+        integerDobbeltLenketListe.leggInn(10);
+        integerDobbeltLenketListe.leggInn(20);
+        integerDobbeltLenketListe.leggInn(30);
+        integerDobbeltLenketListe.leggInn(40);
+        integerDobbeltLenketListe.leggInn(50);
+        Integer x = integerDobbeltLenketListe.hent(4);*/
+
+       // DobbeltLenketListe<String> stringDobbeltLenketListe = new DobbeltLenketListe<>(new String[]{"a", "b", "c"});
+       // System.out.println("hi");
+
+        String[] s = { "Ole" , null , "Per" , "Kari ", null };
+        Liste<String> liste = new DobbeltLenketListe<>(null);
+        System. out .println(liste. antall () + " " + liste. tom ());
     }
 }
 
