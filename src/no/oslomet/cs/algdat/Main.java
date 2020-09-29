c3dfec3e5a629cae4d90ee5848d5ce63aa415f09
Hamzeh0086 <s339572@oslomet.no>
2020-09-29 10:29:23 +0200

    Oppgave 8 og 9 etter lab ( Hamzeh)

--- src/no/oslomet/cs/algdat/Main.java
+++ src/no/oslomet/cs/algdat/Main.java
@@ -4,13 +4,11 @@ import java.util.Arrays;
 
 public class Main {
     public static void main (String[] args) {
-
-        Character[] c = { 'A' , 'B' , 'C' , 'D' , 'E' , 'F' , 'G' , 'H' , 'I' , 'J' ,};
-        DobbeltLenketListe<Character> liste = new DobbeltLenketListe<>(c);
-        System. out .println(liste.subliste(3,8)); // [D, E, F, G, H]
-        System. out .println(liste.subliste(5,5)); // []
-        System. out .println(liste.subliste(8,liste.antall()));
-        
+        String[] navn = { "Lars" , "Anders" , "Bodil" , "Kari" , "Per" , "Berit" };
+        Liste<String> liste = new DobbeltLenketListe<>(navn);
+        liste.forEach(s -> System. out .print(s + " " ));
+        System. out .println();
+        for (String s : liste) System. out .print(s + " " );
     }
 }
 
