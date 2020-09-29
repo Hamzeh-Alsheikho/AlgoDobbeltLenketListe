9c8f8874593582b8219e1d896863988d29bed09d
Hitomi-jp <hitomi.hoshino@gmail.com>
2020-09-29 10:24:29 +0200

    Oppgave 5j: Feil i metoden leggInn()!
    Oppgave6 Test
    java.lang.NullPointerException
    
    Oppgave 3a: Dette (2233 ms) gikk altfor sakte!
                Har du kodet metoden finnNode() riktig?
    
    Fail Test 3, 5, 6

--- src/no/oslomet/cs/algdat/Main.java
+++ src/no/oslomet/cs/algdat/Main.java
@@ -4,6 +4,12 @@ import java.util.Arrays;
 
 public class Main {
     public static void main (String[] args) {
+
+        Character[] c = { 'A' , 'B' , 'C' , 'D' , 'E' , 'F' , 'G' , 'H' , 'I' , 'J' ,};
+        DobbeltLenketListe<Character> liste = new DobbeltLenketListe<>(c);
+        System. out .println(liste.subliste(3,8)); // [D, E, F, G, H]
+        System. out .println(liste.subliste(5,5)); // []
+        System. out .println(liste.subliste(8,liste.antall()));
         
     }
 }
